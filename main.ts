a03f3d1928323c349878757b1e32c091ea8d83c3
tyamada99 <tyamada99@gmail.com>
2026-06-06 13:29:49 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,28 @@
+let running = false
+let startTime = 0
+let elapsed = 0
+let tenth = 0
+input.onButtonPressed(Button.A, function () {
+    if (!(running)) {
+        running = true
+        startTime = control.millis()
+        basic.showString("S")
+    } else {
+        running = false
+        elapsed = control.millis() - startTime
+        // 0.1秒単位で表示
+        tenth = Math.round(elapsed / 10) / 100
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showNumber(tenth)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    running = false
+    elapsed = 0
+    basic.clearScreen()
+})
+basic.forever(function () {
+    // アニメーションなしなので何もしない
+    basic.pause(20)
+})
